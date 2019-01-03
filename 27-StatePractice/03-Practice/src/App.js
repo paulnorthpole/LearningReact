cb1d64307e7a75b9824cac56a61604cd4b383004
Paul Charles <PCharles71@RocketMail.com>
2019-01-02 16:18:01 -0900

    State Practice attempt 3

---
+++ 27-StatePractice/03-Practice/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: "Pablo",
+      age: 65
+    }
+  }
+  render () {
+    return (
+      <div>
+        <h1>{ this.state.name }</h1>
+        <h3>{ this.state.age } years old</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
