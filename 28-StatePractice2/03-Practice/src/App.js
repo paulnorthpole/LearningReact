d4ef138b5fc10c43ae92738a49f576a52ce27749
Paul Charles <PCharles71@RocketMail.com>
2019-01-03 14:08:19 -0900

    Add Practice-03

---
+++ 28-StatePractice2/03-Practice/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {isLoggedIn: false}
+  }
+
+  render () {
+    const isLoggedIn = this.state.isLoggedIn
+    return (
+      <div>
+        <h1>You are currently logged {isLoggedIn ? 'in' : 'out'}</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App
